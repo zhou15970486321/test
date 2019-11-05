5f5339b1e55acb87af94a8f067d52ca2b4d8da99
周浩 <15970486321@163.com>
2019-11-05 17:41:43 +0800

    create aa.java

---
+++ aa.java
@@ -0,0 +1,10 @@
+public  class aa{
+	public static void main(String[] args) {
+		for(int i =1;i<=9;i++){
+			for(int j =1;i<=j;j++){
+				System.out.print(j+"*"+i+"="+i*j+"\n");	
+			}
+		}
+		
+	}
+}
